0503348a9afc8b5c5bf60583a5df16cdd763794e
sekeerVs <jomartolentino2002@gmail.com>
2025-01-10 10:19:14 +0800

    finished

--- online_mongo_try/server/db.js
+++ online_mongo_try/server/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(
-            'mongodb+srv://admin_jomar:test123@cluster0.j9l7y.mongodb.net/testdb?retryWrites=true&w=majority&appName=Cluster0');
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
